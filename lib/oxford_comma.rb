7622cc0aea69002914d422a1c513fdb5624ad2e1
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-30 01:08:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,13 @@
 def oxford_comma(array)
-  last = array.pop
-  first = array.join(", ")
-  return first << ", and #{last}"
+  if array.length == 1 
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    last = array.pop
+    first = array.join(", ")
+    return first << ", and #{last}"
+  end
+    
 
 end
