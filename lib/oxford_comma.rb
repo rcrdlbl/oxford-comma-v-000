4c67a187ac9abf28ff6945a3461124e4c76c23bd
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-30 01:08:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    last = array.pop
+    first = array.join(", ")
+    return first << ", and #{last}"
+  end
+
 
 end
